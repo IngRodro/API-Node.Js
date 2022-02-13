399bbeb130e91a8a0af41bf233f2f118b051a93e
Rodrigo Ramirez <62961147+IngRodro@users.noreply.github.com>
2022-02-12 19:19:38 -0600

    Login, Guardado de Usuario y Encriptacion

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+//Modules
 const morgan = require('morgan');
 const express = require('express');
-var mysql = require('mysql');
+const mysql = require('mysql');
+const bcrypt = require("bcryptjs");
+
 
 const app = express();
 
@@ -10,7 +13,6 @@ var connection = mysql.createConnection({
     password : 'root',
     database : 'restaurant'
   });
-
 
   connection.connect(err =>{
       if(err){throw err};
@@ -23,12 +25,46 @@ app.set('port', 5000);
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    let query = 'SELECT * from users'
-    connection.query(query, function (error, results, fields) {
+app.get('/login',(req, res)=>{
+    let user = req.body;
+    const query = `SELECT * from users where users.username = ?;`;
+    const values = [user.username];
+
+    connection.query(query, values ,function (error, results, fields) {
         if (error) throw error;
-        res.send(results);
+        if(results.length === 1){
+            bcrypt.compare(user.password, results[0].password, (err, match) => {
+                if (err) {
+                    console.log("Error comprobando:", err);
+                } else {
+                   if(match){
+                        const resJson = {
+                            user: results[0].username,
+                            rol : results[0].rol
+                        }
+                        res.send(resJson);
+                   }else{
+                    res.send('Error de sesion')
+                   }
+                }
+            });
+        }else{
+            res.send('Error de sesion')
+        }
+    });
+});
+
+app.post('/user/save',(req, res)=>{
+    let user = req.body;
+    let sql =  `INSERT INTO users(username, name, lastname, age, number, email, password, rol, state) VALUES(?,?,?,?,?,?,?,?,?);`
+    let values = [user.username, user.name, user.lastname, user.age, user.number, user.email, bcrypt.hashSync(user.password, 12), user.rol, user.state]
+    connection.query(sql,values, err =>{
+        if(err){
+            throw err
+        };
+        res.send('User save');
     });
 });
 
